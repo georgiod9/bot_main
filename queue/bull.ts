e15c75d40fffc78c2c6006096d0497296add027d
georgiod9 <georgiod9@gmail.com>
2024-06-25 18:39:57 +0300

    track tasks and save to db + manage redis task queue

--- queue/bull.ts
+++ queue/bull.ts
@@ -5,34 +5,148 @@ import Bull from "bull"
 const redisOptions = { redis: { host: REDIS_HOST, port: parseInt(REDIS_PORT, 10), password: REDIS_PASSWORD, keyPrefix: "{bump}:" } }
 const bumpQueue = new Bull("bump", redisOptions)
 
+
+bumpQueue.on('error', (error) => {
+    console.error('Redis connection error:', error);
+});
+
+bumpQueue.on('stalled', (job) => {
+    console.error('Job stalled:', job.id);
+});
+
+bumpQueue.on('waiting', (jobId) => {
+    console.log('Job waiting:', jobId);
+});
+
+bumpQueue.on('active', (job, jobPromise) => {
+    console.log('Job active:', job.id);
+});
+
+bumpQueue.on('completed', (job, result) => {
+    console.log('Job completed with result:', result);
+});
+
+bumpQueue.on('failed', (job, err) => {
+    console.error('Job failed with error:', err);
+});
+
+
+
+async function checkQueueLength() {
+    const waitingCount = await bumpQueue.getWaitingCount();
+    const activeCount = await bumpQueue.getActiveCount();
+    const completedCount = await bumpQueue.getCompletedCount();
+    const failedCount = await bumpQueue.getFailedCount();
+    const delayedCount = await bumpQueue.getDelayedCount();
+
+    console.log(`Waiting: ${waitingCount}, Active: ${activeCount}, Completed: ${completedCount}, Failed: ${failedCount}, Delayed: ${delayedCount}`);
+}
+
+
+
+export async function pingExecService() {
+    console.log(`Pinging exec service...`)
+    await checkQueueLength();
+
+    bumpQueue.add({ task: "ping", orderId: `pinging...` }, { delay: 0 })
+        .then(job => {
+            console.log(`Job added with ID: ${job.id}`);
+        })
+        .catch(err => {
+            console.error(`Failed to add job: ${err.message}`);
+        });
+
+}
+
+const calculateExpectedEndDate = (delay: number) => {
+    const nowMs = new Date().getTime();
+    const expectedEndMs = nowMs + delay;
+    return new Date(expectedEndMs);
+}
+
+// Duration in hours
 export async function addOrderTasks(orderID: number, token: string, frequency: number, duration: number, funding: number) {
     let delay = 0
+    let tasks = [];
+    const startTime = new Date().getTime();
+
     const PKS = await getWalletEncodedPrivateKeysForOrderId(orderID)
     if (!PKS) {
         console.error("\u001b[1;31m" + '> ERROR ' + "\u001b[0m" + `impossible to extract private keys for order ${orderID}`)
         return
     }
     bumpQueue.add({ task: "set_order_status_live", orderId: `#${orderID.toString()}`, param: orderID }, { delay })
+
+    // Debug tasks
+    tasks.push({ orderId: orderID, taskName: "set_order_status_live", taskNumber: tasks.length + 1, totalDelayMs: delay, expectedEndDate: calculateExpectedEndDate(delay) })
+
     delay += 1000
     bumpQueue.add({ task: "split", orderId: `#${orderID.toString()}`, param: orderID, funding }, { delay })
+
+    // Debug tasks
+    tasks.push({ orderId: orderID, taskName: "split", taskNumber: tasks.length + 1, totalDelayMs: delay, expectedEndDate: calculateExpectedEndDate(delay) })
     delay += 10500
+
+    const durationMs = duration * 60 * 60 * 1000;
+    const actualEndTimeMs = new Date().getTime() + durationMs;
+
     for (let i = 0; i < PKS.length; i++) {
         bumpQueue.add({ task: "buy_token", orderId: `#${orderID.toString()}`, param: orderID, skcrypted: PKS[i], token, amountSol: ((funding / PKS.length) / 2) }, { delay })
+        // Debug tasks
+        tasks.push({ orderId: orderID, taskName: "buy_token", taskNumber: tasks.length + 1, totalDelayMs: delay, expectedEndDate: calculateExpectedEndDate(delay), assignedBot: i })
+
         delay += frequency * 1000
     }
     const IMS = frequency * 1000
     const NIS = Math.floor(duration * 3600000 / IMS)
+    // let forceEnd = false;
     for (let j = 0; j < NIS; j++) {
+        const taskDelay = delay + j * IMS;
+        const expectedEndTimeMs = startTime + taskDelay;
+
+        // Break out of the task distribution loop
+        if (expectedEndTimeMs > actualEndTimeMs) {
+            // forceEnd = true;
+            break;
+        }
+
+        // Create the buy/sell task
         const ri = Math.floor(Math.random() * PKS.length);
         const rt = Math.random() < 0.5 ? "buy_token_random" : "sell_token_random"
-        bumpQueue.add({ task: rt, orderId: `#${orderID.toString()}`, param: orderID, skcrypted: PKS[ri], token }, { delay: delay + j * IMS })
+        bumpQueue.add({ task: rt, orderId: `#${orderID.toString()}`, param: orderID, skcrypted: PKS[ri], token }, { delay: taskDelay })
+
+        // Debug tasks
+        tasks.push({ orderId: orderID, taskName: rt, taskNumber: tasks.length + 1, totalDelayMs: delay, expectedEndDate: calculateExpectedEndDate(taskDelay), assignedBot: ri })
     }
+
+
     delay += duration * 3600000
+
+    // const durationMs = duration * 60 * 60 * 1000;
+    // const actualEndTimeMs = new Date().getTime() + durationMs;
+    const delayDrift = ((Math.abs(delay - actualEndTimeMs)) / delay) * 100;
+
+    // Force send the `withdraw`
+    if (delayDrift > 5) {
+        delay = actualEndTimeMs - new Date().getTime();
+        // bumpQueue.add({ task: "withdraw", orderId: `#${orderID.toString()}`, param: orderID }, { delay: actualEndTimeMs - new Date().getTime() })
+
+    }
+    // else {
+    //     bumpQueue.add({ task: "withdraw", orderId: `#${orderID.toString()}`, param: orderID }, { delay })
+
+    // }
     bumpQueue.add({ task: "withdraw", orderId: `#${orderID.toString()}`, param: orderID }, { delay })
+    // Debug tasks
+    tasks.push({ orderId: orderID, taskName: "withdraw", taskNumber: tasks.length + 1, totalDelayMs: delay, expectedEndDate: calculateExpectedEndDate(delay) })
     delay += 100
     bumpQueue.add({ task: "set_order_status_finished", orderId: `#${orderID.toString()}`, param: orderID }, { delay })
     console.log("\u001b[1;32m" + 'SUCCESS ' + "\u001b[0m" + `adding tasks for order ID #${orderID.toString()}`)
+    console.log(`Total delay applied for order #${orderID.toString()} -> ${delay / 1000}seconds -> ${(delay / 1000) / 60}hrs -> ${delay}ms`)
     console.log('-------------------------------------------------------------')
+
+
+    return { orderId: orderID, startTime: new Date(), totalDelayMs: delay, tasks: tasks }
 }
 
 
